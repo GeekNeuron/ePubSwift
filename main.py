669c2df003dad0fa3bc977816c5cba6af243beab
Geek Neuron <geekneuron@gmail.com>
2025-07-25 18:30:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # main.py
+
 import sys
+import os
+
+# این چند خط کد مشکل را حل می‌کند
+# Add the project's root directory to the Python path
+# This ensures that modules in subdirectories (like ui, database) can be found
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from PySide6.QtWidgets import QApplication
 from ui.main_window import EpubReader
 
