52a2a612a6037d6faf5e93527db0d41db88372dc
Geek Neuron <geekneuron@gmail.com>
2025-07-25 18:27:02 +0330

    Delete hooks directory

--- hooks/hook-PySide6.py
+++
@@ -1,6 +0,0 @@
-# hooks/hook-PySide6.py
-from PyInstaller.utils.hooks import copy_metadata, collect_dynamic_libs
-
-# datas = copy_metadata('PySide6') # Commented out to bypass the metadata error
-binaries = collect_dynamic_libs('PySide6')
-datas = [] # Add this line to ensure 'datas' is defined
