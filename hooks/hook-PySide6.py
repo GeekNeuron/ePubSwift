14961f9779930417ae897fda13fa5da39d6ea237
Geek Neuron <geekneuron@gmail.com>
2025-07-25 18:03:01 +0330

    Update hook-PySide6.py

--- hooks/hook-PySide6.py
+++ hooks/hook-PySide6.py
@@ -1,7 +1,6 @@
 # hooks/hook-PySide6.py
 from PyInstaller.utils.hooks import copy_metadata, collect_dynamic_libs
 
-# This hook is often needed to correctly bundle PySide6's dependencies,
-# especially for networking and SSL which can cause the "ordinal not found" error.
-datas = copy_metadata('PySide6')
+# datas = copy_metadata('PySide6') # Commented out to bypass the metadata error
 binaries = collect_dynamic_libs('PySide6')
+datas = [] # Add this line to ensure 'datas' is defined
